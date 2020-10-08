9fd0a0457fc5781648cb92298c15033f9a6178b6
Delaunay Valentin <valentin.delaunay@bodet.com>
2020-10-08 19:54:34 +0200

    Retrofit, room et recupération des news

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "09de0d6be7a54110aca7d568cd448667"
+        const val BASE_URL = "https://newsapi.org"
+    }
+
+}
